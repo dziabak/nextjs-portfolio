3d00cf84ac16b502294ee0b5968a947a97fbd192
Grzegorz <gd.gdabrowski@gmail.com>
2024-12-13 14:35:26 +0100

    Small layout tweaks

--- app/gamedev/page.tsx
+++ app/gamedev/page.tsx
@@ -18,7 +18,7 @@ const Page = () => {
 			</h1>
 			{/* <p className="mb-8 text-3xl font-bold">Game Designer & QA Specialist</p> */}
 			{/* <Header text="Profile" /> */}
-			<div className="space-y-2 text-lg">
+			<div className="space-y-2 text-lg mb-8">
 				<p>
 					I am a multi-disciplinary professional with expertise in game design,
 					systems development, and quality assurance.
@@ -35,8 +35,11 @@ const Page = () => {
 					throughout development.
 				</p>
 			</div>
+			<DetailsLink link="/experience" text="Check my full CV →" />
+			<Header text="Projects" />
+			<ProjectCardsGrid data={VIDEO_GAME_PROJECTS_DATA} />
 			<Header text="Experience highlights" />
-			<div className="space-y-2 text-lg mb-8">
+			<div className="space-y-2 text-lg">
 				<p>
 					I have contributed to a diverse range of projects, from indie titles
 					to AAA games.
@@ -51,24 +54,21 @@ const Page = () => {
 					At <span className="font-bold">Keywords Studios</span>, I supported
 					the growth of the Katowice FQA office into one of Europe&apos;s
 					largest QA outsource studios and worked on high-profile titles such as{" "}
-					<span className="font-bold">THE FINALS</span>,
+					<span className="font-bold">THE FINALS</span>,{" "}
 					<span className="font-bold">Outriders: Worldslayer</span>, and{" "}
 					<span className="font-bold">The Medium</span> as well as{" "}
-					<span className="font-bold">Harold Halibut</span> and
+					<span className="font-bold">Harold Halibut</span> and{" "}
 					<span className="font-bold">Golf With Your Friends</span>.
 				</p>
 				<p>
 					During my time at <span className="font-bold">Jujubee</span>, I
 					contributed to the development of games like{" "}
 					<span className="font-bold">Truck Simulation 19</span>,{" "}
-					<span className="font-bold">Deep Diving Simulator</span>, and{" "}
+					<span className="font-bold">Deep Diving Simulator</span> and{" "}
 					<span className="font-bold">Take Off: The Flight Simulator</span>,
 					focusing on gameplay systems, UI/UX design, and post-release support.
 				</p>
 			</div>
-			<DetailsLink link="/experience" text="Check my full CV →" />
-			<Header text="Projects" />
-			<ProjectCardsGrid data={VIDEO_GAME_PROJECTS_DATA} />
 		</section>
 	);
 };
